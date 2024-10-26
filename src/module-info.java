118d0e448d26a6892a1d2b5582bc9c04c1b9a3fa
priyadharsini93 <priyapdtrt@gmail.com>
2024-10-26 16:09:15 +0530

    going to commit a new file

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author AVINASH K
+ *
+ */
+module JavaPrograms {
+}
---
+++ src/org/trialPrograms/ConditionalStatement.java
@@ -0,0 +1,16 @@
+package org.trialPrograms;
+
+public class ConditionalStatement {
+
+	public static void main(String[] args) {
+	
+		
+		int a[]= {10,20,30};
+		{
+			for()
+		}
+		
+		
+	}
+
+}
